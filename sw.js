ea2f36d5904a5659e3e32387d70677f9b406908a
Rierie-Fai <irvan.26@gmail.com>
2026-02-23 15:45:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hexindo-cache-v1.5';
+const CACHE_NAME = 'hexindo-cache-v1.6';
 
 // Daftarkan semua file yang ada di folder Anda berdasarkan struktur direktori
 const urlsToCache = [
